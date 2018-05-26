9cecb70ce93a06b9b074454ee7cabdbf96565ca4
JAMES K ARASIM <jaemzware@hotmail.com>
2018-05-26 04:24:00 -0700

    added linux flavors of browsers for running grid on linux through docker

--- src/test/java/com/jaemzware/BrowserType.java
+++ src/test/java/com/jaemzware/BrowserType.java
@@ -10,8 +10,10 @@ public enum BrowserType {
     //GRID: PLATFORM AND VERSION ARE ONLY USED WHEN USING GRID OR A CHROME EMULATION
     //NON-GRID: FOR NON-GRID, JUST SPECIFYING CHROME, FIREFOX, OR SAFARI WORKS ON MAC
     //FIREFOX: ONLY WORKS WITH GECKODRIVER NOW
-    CHROME("chrome","",Platform.MAC),  //REQUIRES CHROMEDRIVER
+    CHROME("chrome","",Platform.MAC),
+    CHROMELINUX("chrome","",Platform.LINUX), //REQUIRES CHROMEDRIVER
     SAFARI("safari","",Platform.MAC), //REQUIRES SAFARI => ADVANCED => DEVELOP => ALLOW REMOTE AUTOMATION, EXTENSION http://selenium-release.storage.googleapis.com/index.html?path=2.48/, CERTIFICATE https://itisatechiesworld.wordpress.com/2015/04/15/steps-to-get-selenium-webdriver-running-on-safari-browser/
+    FIREFOXLINUX("firefox","",Platform.LINUX),
     FIREFOX("firefox","",Platform.MAC); //REQUIRES GECKODRIVER
     
     public final String browserName;
