00ae04f4d0b0b5ad5bae09a802e2cea0215f9307
JAMES K ARASIM <jaemzware@hotmail.com>
2019-01-11 18:26:57 -0800

    added load more button and page not found check

--- src/test/java/com/jaemzware/misc/MultiThreadedAllInternalLinks.java
+++ src/test/java/com/jaemzware/misc/MultiThreadedAllInternalLinks.java
@@ -1,9 +1,7 @@
 package com.jaemzware.misc;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.sun.tools.corba.se.idl.constExpr.Not;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,12 +10,14 @@ import java.util.Hashtable;
 import java.util.List;
 
 public class MultiThreadedAllInternalLinks extends Thread {
-    private final int numBrowsers = 7;
+    private final int numBrowsers = 5;
     private static int semaphore = 0;
     private String seed = "https://www.soundtransit.org";
     private String seekHrefStartsWith = "/";
+    private String loadMoreButtonCssSelector = "button[class*='load-more']";
     public static Hashtable<String,String> toVisit = new Hashtable<String,String>();
     public static Hashtable<String,String> visited = new Hashtable<String,String>();
+    public static Hashtable<String,String> pageNotFound = new Hashtable<String,String>();
 
     @BeforeClass
     public static void BeforeClass(){
@@ -28,6 +28,7 @@ public class MultiThreadedAllInternalLinks extends Thread {
     //THIS TEST LAUNCHES MULTIPLE BROWSERS TO VISIT ALL LINKS ON THE CRAIGSLIST HOME PAGE CONCURRENTLY
     @Test
     public void MultiBrowserTest() throws Exception{
+        //launch one browser to get unique urls from a seed page
         WebDriver driver = new ChromeDriver();
         driver.get(seed);
         visited.put(seed, "SEED");
@@ -35,17 +36,26 @@ public class MultiThreadedAllInternalLinks extends Thread {
         System.out.println("INITIAL SIZE:" + toVisit.size());
         driver.quit();
 
+        //launch n browsers to visit urls found and find more on each page, until there are no more unique pages left
         for(int i=0; i<numBrowsers; i++){
             (new MultiThreadedAllInternalLinks()).start();
         }
 
-        //wait until all threads are finished to end the test
+        //wait until all browsers/threads are finished to end the test
         while(semaphore > 0){
-            Thread.sleep(1000);
+            Thread.sleep(5000);
+        }
+
+        //report pages not found
+        for(String key:pageNotFound.keySet()) {
+            System.out.println("PAGE NOT FOUND: "+key+" REFERRER: " + pageNotFound.get(key));
         }
 
     }
 
+    /**
+     * This is the thread function that launches a browser, and visits unique pages until none are found.  While it's running, it records pages that were not found.
+     */
     public void run(){
         semaphore++;
 
@@ -55,10 +65,13 @@ public class MultiThreadedAllInternalLinks extends Thread {
 
         do {
             href = GetHrefToVisit();
-            if(href != null && href[0].startsWith(seed)) { //NOTE: FOUND AN HREF ON THE SITE THAT BEGAN WITH //KINGCOUNTY.GOV, AND I DON'T WANT TO FOLLOW THOSE. I THINK ITS A BUG ON PAGE https://www.soundtransit.org/blog/platform/get-ready-seattle-squeeze
+            if(href != null) {
                 System.out.println("VISITING: " + href[0] + " FROM: " + href[1]);
                 driver.get(href[0]);
-                GetUniqueHrefs(driver, href[0]);
+                if(!IsPageNotFound(driver,href)) {
+                    //don't get unique hrefs if the page is not found. this will avoid countless search pages
+                    GetUniqueHrefs(driver, href[0]);
+                }
             }
         } while(href != null);
 
@@ -67,12 +80,26 @@ public class MultiThreadedAllInternalLinks extends Thread {
         semaphore--;
     }
 
+    /**
+     * Gather a list of unique hrefs that are not documents, and have not yet been marked "tovisit" or "visited".
+     * @param driver - webdriver being used
+     * @param currentHref - current href navigated to.
+     */
     public void GetUniqueHrefs(WebDriver driver, String currentHref) {
+        CheckAndClickLoadMoreButton(driver);
         List<WebElement> hrefs = driver.findElements(By.cssSelector("a[href^='"+seekHrefStartsWith+"']"));
         for(WebElement we:hrefs) {
             try {
                 String href = we.getAttribute("href");
-                if (!toVisit.containsKey(href) && !visited.containsKey(href)) {
+                if (
+                        href.startsWith(seed) &&
+                        !href.endsWith(".docx") &&
+                        !href.endsWith(".ics") &&
+                        !href.endsWith(".pptx") &&
+                        !href.endsWith(".zip") &&
+                        !toVisit.containsKey(href) &&
+                        !visited.containsKey(href)
+                        ) {
                     toVisit.put(href, currentHref);
                 }
             } catch (StaleElementReferenceException serex) {
@@ -82,10 +109,14 @@ public class MultiThreadedAllInternalLinks extends Thread {
         }
     }
 
+    /**
+     * Get one of the unique hrefs gathered to visit, and move it from "toVisit" to "visited".
+     * @return
+     */
     public String[] GetHrefToVisit() {
         String[] hrefToVisit = new String[2];
 
-        System.out.println("SIZE:" + toVisit.size() + " " + visited.size());
+        System.out.println("TOVISIT: " + toVisit.size() + " VISITED: " + visited.size());
 
         if(toVisit.isEmpty()) {
             return null;
@@ -97,6 +128,43 @@ public class MultiThreadedAllInternalLinks extends Thread {
                 break;
             }
             return hrefToVisit;
+        }
+    }
+
+    /**
+     * If there's a load more button on the page, click it until it's gone, for a maximum of 100 clicks.
+     * @param driver
+     */
+    public void CheckAndClickLoadMoreButton(WebDriver driver) {
+        int max = 100;
+        while(driver.findElements(By.cssSelector(loadMoreButtonCssSelector)).size() > 0 && --max > 0) {
+            ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+            WebElement loadMoreButton = driver.findElement(By.cssSelector(loadMoreButtonCssSelector));
+            if(!loadMoreButton.isDisplayed()) {
+                break;
+            } else {
+                loadMoreButton.click();
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ex) {
+                }
+            }
+        }
+    }
+
+    /**
+     * Check if the current page is a 404 page.  If it is, added to the pageNotFound hashtable for later reporting and return true.
+     * @param driver
+     * @param href array where first value is current url and second value is where the url was found.
+     * @return true if page is a 404 page
+     */
+    public boolean IsPageNotFound(WebDriver driver, String[]href) {
+        if(driver.findElements(By.xpath("//title[contains(text(),'Page not found')] | //title[contains(text(),'404 Not Found')]")).size() >= 1) {
+            pageNotFound.put(href[0],href[1]);
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
